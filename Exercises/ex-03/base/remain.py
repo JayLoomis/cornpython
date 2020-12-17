d4569203dca3b36ed2c25291e5948afc55fd9575
Andrew Henderson <graypawn@Andrews-MacBook-Pro-2.local>
2020-12-17 14:21:40 -0800

    finished verifyemail() and finished module, ready for next Revision

--- Exercises/ex-03/base/remain.py
+++ Exercises/ex-03/base/remain.py
@@ -17,25 +17,42 @@ def search():
     main()
 
 def verifyphone(p):
+    #initialize starting input:  p
     startnum = p
+    #initialize an empty string to catch the number we need.
     outnum = ""
+    #goodnums is a list of just the digits we want to accept.
     goodnums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
+    #for loop goes through their string and adds only the digits of goodnums to the output variable
     for num in startnum:
         if num in goodnums:
             outnum += num
         else:
             continue
+
+    if len(outnum) > 10:
+        outnum = "Error:  Too many numbers."
+    else:
+        outnum = "({}) {}-{}".format(outnum[:3],outnum[3:6],outnum[6:])
+
     return outnum
 
-def verifyemail():
+def verifyemail(e):
+    #slice off the last four letters
+    dotcom = e[-4::1]
+    
+    #list of Top Level Domains that don't suck
+    tld_list = (".com",".org",".net",".int",".edu",".gov",".mil")
 
-    return
-
-def createrecord():
-    #Create the Record as a Dictionary
-    a_record = dict(f_name=f_name, l_name=l_name, phone=phone, email=email)
-    return
+    #check for @ and .something
+    if "@" not in e:
+        print("Error - no At Symbol")
+    elif dotcom not in tld_list:
+        print("not a dotcom")
+    #else:
+        #e = "Great"
+    return e
 
 def main():
     #defining options for this program
@@ -70,11 +87,14 @@ def main():
                 phone = input("\nType Phone Number:\n")
                 email = input("\nType Email Address:\n")
 
-                verifyphone(phone)
+                #verify phone drops whatever format they have and replaces it, counts numbers
+                phone = verifyphone(phone)
 
-                verifyemail(email)
+                #verify TLD and that there is an @ symbol.
+                #currently no verification for multiple @ symbols.
+                email = verifyemail(email)
 
-                createrecord(f_name, l_name, phone, email)
+                a_record = dict(f_name=f_name, l_name=l_name, phone=phone, email=email)
                 
                 #print a verification of the Dictionary values:
                 print("\nYour new employee data is:")
