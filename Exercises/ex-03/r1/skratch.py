3057934861916483852e2cf6ef935e769c8ed384
Andrew Henderson <graypawn@Andrews-MacBook-Pro-2.local>
2021-01-06 21:19:57 -0800

    Probably going to drop this one.

--- Exercises/ex-03/r1/skratch.py
+++ Exercises/ex-03/r1/skratch.py
@@ -3,9 +3,11 @@ import random
 import datetime
 
 def verifyday(d):
-    if d.isdigit() > 31:
+    if d.isdigit() == False:
+        raise ValueError("Please enter a numeral")
+    elif int(d) > 31:
         raise ValueError("Day number is too high.")
-    elif d.isdigit() < 1:
+    elif int(d) < 1:
         raise ValueError("Day must be between 1 and 31.")
 
     return d
@@ -26,7 +28,7 @@ def verifymonth(m):
 
 def verifyyear(y):   
     # 
-    if y > datetime.date.year():
+    if int(y) > datetime.datetime.today().year:
         raise ValueError("Date cannot be in the future.")
     else:
         # returns the year
@@ -34,9 +36,8 @@ def verifyyear(y):
 
 def main():
     contactdate = None
-    badmonth = None
-    wrongyear = None
-
+    baddate = None
+    
     verified = False
     while verified == False:
         # input the date of contact
@@ -46,43 +47,60 @@ def main():
         if inputtoday == "Y":
             contactdate = datetime.date.today()
         else:
-            datecheck = False
-            while datecheck == False:
-                # get the day
+            # input the day and check to make sure is between 1 and 31
+            inputting = True
+            while inputting:
                 d = input("Day?\n")
-                # get and check the month
-                inputting = True
-                while inputting:
-                    m = input("Month?\n")
-                    try:
-                        # verify month takes in a STR and returns a format code
-                        monthformat = verifymonth(m)
-                    except ValueError as e:
-                        badmonth = e
-                    if badmonth == None:
-                        inputting = False
-                    else:
-                        print(badmonth)
-                        badmonth = None
-                        continue
+                try:
+                    #verifies day is between 1 and 31
+                    d = verifyday(d)
+                except ValueError as e:
+                    baddate = e
+                if baddate == None:
+                    inputting = False
+                else:
+                    print(baddate)
+                    baddate = None
+                    continue
+            
+            # get and check the month
+            inputting = True
+            while inputting:
+                m = input("Month?\n")
+                try:
+                    # verify month takes in a STR and returns a format code
+                    monthformat = verifymonth(m)
+                except ValueError as e:
+                    baddate = e
+                if baddate == None:
+                    print(monthformat)
+                    inputting = False
+                else:
+                    print(baddate)
+                    baddate = None
+                    continue
 
-                # get and check to make sure the year isn't in the future
-                inputting = True
-                while inputting:
-                    y = input("Year?\n")
-                    try:
-                        verifyyear(y)
-                    except ValueError as e:
-                        wrongyear = e
-                    if wrongyear == None:
-                        inputting = False
-                    else:
-                        print(wrongyear)
-                        wrongyear = None
-                        continue
+            # get and check to make sure the year isn't in the future
+            inputting = True
+            while inputting:
+                y = input("Year?\n")
+                try:
+                    y = verifyyear(y)
+                except ValueError as e:
+                    baddate = e
+                if baddate == None:
+                    inputting = False
+                else:
+                    print(baddate)
+                    baddate = None
+                    continue
 
-# https://stackoverflow.com/questions/466345/converting-string-into-datetime
-
+        # just here to show that the input is working.
+        print(d, monthformat, y)
+        # build the date as a datetime object
+        # https://stackoverflow.com/questions/466345/converting-string-into-datetime
+        
+        # Input notes and verify the information input
         contactnote = input("\nAdd any notes about your contact\n")
         print(f"\nYou're entering:\n--> {contactnote}\nas happening on:  {contactdate}")
         
