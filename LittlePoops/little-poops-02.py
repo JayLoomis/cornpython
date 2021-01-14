369812c5a8983e73680f6936bd226cfe6963bcda
Andrew Henderson <graypawn@Andrews-MacBook-Pro-2.local>
2021-01-14 13:08:33 -0800

    Initializing little poops 2

---
+++ LittlePoops/little-poops-02.py
@@ -0,0 +1,5 @@
+# Write a function that takes two strings and determines whether the second 
+# is an anagram of the first. Return an appropriate Boolean.
+
+# Because anagrams are often multiple words, remove all non-printing 
+# characters from both params before you do your check.
