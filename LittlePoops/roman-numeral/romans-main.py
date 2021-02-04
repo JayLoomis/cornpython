eaa0cb9700d8bb037a38bf95620b66565d8330ba
Andrew Henderson <graypawn@Andrews-MacBook-Pro-2.local>
2021-02-04 14:40:22 -0800

    Initializing Roman Numerals Little Poop

---
+++ LittlePoops/roman-numeral/romans-main.py
@@ -0,0 +1,8 @@
+def main():
+    valid_num = False:
+    while valid_num == False:
+        print("Enter an Arabic or Roman Numeral:\n")
+        print("or type Q to quit\n")
+        num = input()
+
+if __name__ == "__main__" : main()
---
+++ LittlePoops/roman-numeral/romans.py
@@ -0,0 +1,10 @@
+class RomanNumerals:
+    
+    def arabic_to_roman(self, arabic):
+        pass
+
+    def roman_to_arabic(self, roman):
+        pass
+
+    def verify_roman(self, roman):
+        pass
