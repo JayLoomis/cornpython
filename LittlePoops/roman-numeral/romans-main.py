8d00f33894a4b4b711f20a3bb35b1ecaa453abff
Andrew Henderson <graypawn@Andrews-MacBook-Pro-2.local>
2021-02-04 21:26:03 -0800

    Starting work using my beta version of a generic interface loop

--- LittlePoops/roman-numeral/romans-main.py
+++ LittlePoops/roman-numeral/romans-main.py
@@ -1,8 +1,32 @@
+import sys
+import romans
+
+def verify_opt(opt):
+    if opt.isnumeric() == True:
+        # print("*") # print test
+        return True
+    #elif RomanNumerals.verify_roman(opt) == True:
+    #    return True
+    else:
+        return False
+
+
 def main():
-    valid_num = False:
-    while valid_num == False:
-        print("Enter an Arabic or Roman Numeral:\n")
-        print("or type Q to quit\n")
-        num = input()
+    print("\n\n***Roman/Arabic alternator***")
+    valid_opt = False
+    while valid_opt == False:
+        print("\nEnter an Arabic or Roman neumeral:")
+        print("(or type Q to quit)\n")
+        opt = input()
+        if opt == "Q":
+            sys.exit("\n\nThank you.")
+
+        elif verify_opt(opt) == False:
+            print("\n'{}' not recognized.".format(opt))
+            continue
+
+        elif verify_opt(opt) == True:
+            # suite
+            valid_opt = True
 
 if __name__ == "__main__" : main()
